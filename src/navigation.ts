180ef10edb0e295b75671df2a035683a0e6e59e2
Vpgx7 <vincentpgiacomo@gmail.com>
2025-07-16 20:15:21 -0400

    Fix social media links - add https:// to LinkedIn URL

--- src/navigation.ts
+++ src/navigation.ts
@@ -122,8 +122,8 @@ export const footerData = {
   links: [],
   secondaryLinks: [],
   socialLinks: [
-    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'YOUR_LINKEDIN_URL' },
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'YOUR_X_URL' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/vincent-giacomo' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/epheiaai?s=21&t=ilfdQ3AaaMK_1aro6wuhwg' },
   ],
   footNote: `© 2025 Epheia Inc. • Epheia™ is a trademark of Epheia Inc.`,
 };
